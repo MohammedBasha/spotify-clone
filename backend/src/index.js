8d2b4aba275d70d08ab52105a631e6af559fb7fa
MohammedBasha <me@mohammedbasha.com>
2026-01-07 03:54:00 +0200

    Finishing the Not found page + the deployment initialization

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,13 @@ import { clerkMiddleware } from "@clerk/express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import cors from "cors";
-
+import fs from "fs";
 import { createServer } from "http";
+import cron from "node-cron";
+
 import { initializeSocket } from "./lib/socket.js";
 
 import { connectDB } from "./lib/db.js";
-
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -19,17 +20,17 @@ import statRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
+const app = express();
+const PORT = process.env.PORT;
 
 const httpServer = createServer(app);
 initializeSocket(httpServer);
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
-        credential: true,
+        origin: "http://localhost:3000",
+        credentials: true,
     })
 );
 
@@ -46,7 +47,22 @@ app.use(
     })
 );
 
-// routes
+// cron jobs
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+    if (fs.existsSync(tempDir)) {
+        fs.readdir(tempDir, (err, files) => {
+            if (err) {
+                console.log("error", err);
+                return;
+            }
+            for (const file of files) {
+                fs.unlink(path.join(tempDir, file), (err) => {});
+            }
+        });
+    }
+});
+
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
@@ -54,6 +70,16 @@ app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../frontend", "dist", "index.html")
+        );
+    });
+}
+
+// error handler
 app.use((err, req, res, next) => {
     res.status(500).json({
         message:
@@ -64,6 +90,6 @@ app.use((err, req, res, next) => {
 });
 
 httpServer.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log("Server is running on port " + PORT);
     connectDB();
 });
